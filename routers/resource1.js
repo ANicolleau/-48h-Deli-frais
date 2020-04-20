8b655d43b59720bde8750ee21d76037737099e9f
Antostef <35449646+Antostef@users.noreply.github.com>
2020-04-20 19:09:02 +0200

    views routes

--- routers/resource1.js
+++ routers/resource1.js
@@ -3,6 +3,98 @@ const express = require('express')
 const router = express.Router()
 
 
+//
+// User
+//
+
+router.get(`/users/:id`, async (request, response) => {
+    console.log("Hello")
+
+    response.format({
+        html: () => {
+            response.render('resources/users/user')
+        },
+        json: () => {
+            response.send({
+                bienvenue: "à toi",
+                jesuis: "la réponse json 1"
+            })
+        }
+    })
+})
+
+//
+// Producers
+//
+
+router.get(`/producers/:id`, async (request, response) => {
+    console.log("Hello")
+
+    response.format({
+        html: () => {
+            response.render('resources/products/product')
+        },
+        json: () => {
+            response.send({
+                bienvenue: "à toi",
+                jesuis: "la réponse json 1"
+            })
+        }
+    })
+})
+
+router.get('/producers', async (request, response) => {
+    console.log("Hello")
+
+    response.format({
+        html: () => {
+            response.render('resources/products/products')
+        },
+        json: () => {
+            response.send({
+                bienvenue: "à toi",
+                jesuis: "la réponse json 1"
+            })
+        }
+    })
+})
+
+//
+// Products
+//
+
+router.get(`/product/:id`, async (request, response) => {
+    console.log("Hello")
+
+    response.format({
+        html: () => {
+            response.render('resources/products/product')
+        },
+        json: () => {
+            response.send({
+                bienvenue: "à toi",
+                jesuis: "la réponse json 1"
+            })
+        }
+    })
+})
+
+router.get('/products', async (request, response) => {
+    console.log("Hello")
+
+    response.format({
+        html: () => {
+            response.render('resources/products/products')
+        },
+        json: () => {
+            response.send({
+                bienvenue: "à toi",
+                jesuis: "la réponse json 1"
+            })
+        }
+    })
+})
+
 router.get('/connection', async (request, response) => {
     console.log("Hello")
 
