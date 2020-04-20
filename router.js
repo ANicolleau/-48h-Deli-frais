8e246bba82c90944c3147bd007fb95b1a4cb87d7
Antostef <35449646+Antostef@users.noreply.github.com>
2020-04-20 18:12:58 +0200

    rework routes, added extra form options

--- router.js
+++ router.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const resource1Router = require('./routers/resource1')
-const resource2Router = require('./routers/resource2')
 
 const router = express.Router()
 
-router.use('/resource1', resource1Router)
-router.use('/resource2', resource2Router)
+router.use('/', resource1Router)
 
 module.exports = router
--- routers/resource1.js
+++ routers/resource1.js
@@ -2,12 +2,45 @@ const express = require('express')
 
 const router = express.Router()
 
+
+router.get('/connection', async (request, response) => {
+    console.log("Hello")
+
+    response.format({
+        html: () => {
+            response.render('login/connection')
+        },
+        json: () => {
+            response.send({
+                bienvenue: "à toi",
+                jesuis: "la réponse json 1"
+            })
+        }
+    })
+})
+
+router.get('/register', async (request, response) => {
+    console.log("Hello")
+
+    response.format({
+        html: () => {
+            response.render('login/register')
+        },
+        json: () => {
+            response.send({
+                bienvenue: "à toi",
+                jesuis: "la réponse json 1"
+            })
+        }
+    })
+})
+
 router.get('/', async (request, response) => {
     console.log("Hello")
 
     response.format({
         html: () => {
-            response.render('resource1/home')
+            response.render('home')
         },
         json: () => {
             response.send({
