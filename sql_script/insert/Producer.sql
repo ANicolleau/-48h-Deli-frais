c34aaf9cb5eed6dec62a99eceb26d806af3f2e87
antoinenicolleau <antoine.nicolleau@ynov.com>
2020-04-20 15:17:26 +0200

    Adding values in database

---
+++ sql_script/insert/Producer.sql
@@ -0,0 +1,13 @@
+INSERT INTO Producer(description, category_id) VALUES ("Nous produisons du fromage de chèvres et de brebis.", 1);
+INSERT INTO Producer(description, category_id) VALUES ("Nous produisons essentiellement du fromage de brebis.", 1);
+INSERT INTO Producer(description, category_id) VALUES ("Nous produisons du fromage de vache et de brebis.", 1);
+INSERT INTO Producer(description, category_id) VALUES ("Nous produisons du fromage et du lait de vache.", 1);
+INSERT INTO Producer(description, category_id) VALUES ("Nous ne produisons que du lait de vache.", 1);
+INSERT INTO Producer(description, category_id) VALUES ("Nous pouvons vendre des pommes, poires et orange. Nos arbes fruitiers sont sous serres.", 2);
+INSERT INTO Producer(description, category_id) VALUES ("Nous pouvons vendre des raisins et des fraises. Les fraisiers se situent sous serres.", 2);
+INSERT INTO Producer(description, category_id) VALUES ("Notre serre posséde essentiellement des pommiers.", 2);
+INSERT INTO Producer(description, category_id) VALUES ("Notre ferme fruitière vous propose toutes sortes d'agrumes. Orange, pamplemousse, citron, mandarine, clémentine.", 2);
+INSERT INTO Producer(description, category_id) VALUES ("Notre proposons des concombres, courgettes, pommes de terre.", 3);
+INSERT INTO Producer(description, category_id) VALUES ("Notre vendons des haricots vert, choux fleurs et salades.", 3);
+INSERT INTO Producer(description, category_id) VALUES ("Notre avons des petits pois, des carottes.", 3);
+
